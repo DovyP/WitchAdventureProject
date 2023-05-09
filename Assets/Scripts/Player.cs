549833bfb0fd176a222de433ccb1675e918eda7b
dovpe <translatefunmira@gmail.com>
2023-05-09 07:52:44 +0300

    Refactor input system to use unity's new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,29 +15,11 @@ public class Player : MonoBehaviour
 
     private bool isWalking;
 
+    [SerializeField] private Inputs inputs;
+
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = inputs.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection * movementSpeed * Time.deltaTime;  
         isWalking = moveDirection != Vector3.zero;
