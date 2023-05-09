c64a77162deb545419e8961ff90c8b75f2b77895
dovpe <translatefunmira@gmail.com>
2023-05-09 09:28:43 +0300

    Add interactions with counters

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,13 +9,50 @@ public class Player : MonoBehaviour
     [Header("Physics")]
     [SerializeField] private float playerRadius;
     [SerializeField] private float playerHeight;
+    [SerializeField] private float interactDistance;
 
     private bool isWalking;
+    private Vector3 lastInteractDirection;
 
-    // references
+    [Space(10)]
+    [Header("References")]
     [SerializeField] private Inputs inputs;
+    [SerializeField] private LayerMask countersLayerMask;
 
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = inputs.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDirection != Vector3.zero)
+        {
+            lastInteractDirection = moveDirection;
+        }
+
+        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            // using try for null check
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // has ClearCounter component
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = inputs.GetMovementVectorNormalized();
 
@@ -24,7 +61,7 @@ public class Player : MonoBehaviour
         float moveDistance = moveSpeed * Time.deltaTime;
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
 
-        if(!canMove)
+        if (!canMove)
         {
             // cant move towards move direction
 
@@ -32,23 +69,25 @@ public class Player : MonoBehaviour
             Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
 
-            if(canMove)
+            if (canMove)
             {
                 // can move on X
                 moveDirection = moveDirectionX;
-            } else
+            }
+            else
             {
                 // cannot move on X
 
                 // move on Z
                 Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
-            
+
                 if (canMove)
                 {
                     // can move on Z
                     moveDirection = moveDirectionZ;
-                } else
+                }
+                else
                 {
                     // cant move in any direction
                 }
@@ -63,10 +102,5 @@ public class Player : MonoBehaviour
         isWalking = moveDirection != Vector3.zero;
 
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }
