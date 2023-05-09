eea4255c1e8029e09568917ae5a5ec9043dab85a
dovpe <translatefunmira@gmail.com>
2023-05-09 12:00:15 +0300

    Add function to track items parent (which counter the item is on)

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,11 +4,57 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private HerbloreObjectSO herbloreObjectSO;
     [SerializeField] private Transform counterTop;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private HerbloreObject herbloreObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (herbloreObject != null)
+            {
+                herbloreObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
     public void Interact()
     {
-        Transform herbloreObjectTransform = Instantiate(herbloreObjectSO.prefab, counterTop);
-        herbloreObjectTransform.localPosition = Vector3.zero;
-        Debug.Log(herbloreObjectTransform.GetComponent<HerbloreObject>().GetHerbloreObjectSO().objectName);
+        if (herbloreObject == null)
+        {
+            Transform herbloreObjectTransform = Instantiate(herbloreObjectSO.prefab, counterTop);
+            herbloreObjectTransform.GetComponent<HerbloreObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(herbloreObject.GetClearCounter());
+        }
+    }
+
+    public Transform GetHerbloreObjectFollowTransform()
+    {
+        return counterTop;
+    }
+
+    public void SetHerbloreObject(HerbloreObject herbloreObject)
+    {
+        this.herbloreObject = herbloreObject;
+    }
+
+    public HerbloreObject GetHerbloreObject()
+    {
+        return herbloreObject;
+    }
+
+    public void ClearHerbloreObject()
+    {
+        herbloreObject = null;
+    }
+
+    public bool HasHerbloreObject()
+    {
+        return herbloreObject != null;
     }
 }
--- Assets/Scripts/Herblore/HerbloreObject.cs
+++ Assets/Scripts/Herblore/HerbloreObject.cs
@@ -4,5 +4,28 @@ public class HerbloreObject : MonoBehaviour
 {
     [SerializeField] private HerbloreObjectSO herbloreObjectSO;
 
+    private ClearCounter clearCounter;
+
     public HerbloreObjectSO GetHerbloreObjectSO() { return herbloreObjectSO; }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if(this.clearCounter != null)
+        {
+            this.clearCounter.ClearHerbloreObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if (clearCounter.HasHerbloreObject())
+        {
+            Debug.LogError("Counter already has a HerbloreObject!");
+        }
+
+        clearCounter.SetHerbloreObject(this);
+
+        transform.parent = clearCounter.GetHerbloreObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+    public ClearCounter GetClearCounter() { return clearCounter; }
 }
